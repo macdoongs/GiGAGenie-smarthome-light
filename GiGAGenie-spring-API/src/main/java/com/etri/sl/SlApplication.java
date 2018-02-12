e196a1740bd804349b23beaa891088f46e92dc86
Youngmin Kim <rladudals02@gmail.com>
2018-02-12 21:48:39 +0900

    Spring deployment setting

--- GiGAGenie-spring-API/src/main/java/com/etri/sl/SlApplication.java
+++ GiGAGenie-spring-API/src/main/java/com/etri/sl/SlApplication.java
@@ -2,9 +2,16 @@ package com.etri.sl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SlApplication {
+public class SlApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SlApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SlApplication.class, args);
--- GiGAGenie-spring-API/src/main/java/com/etri/sl/configs/Config.java
+++ GiGAGenie-spring-API/src/main/java/com/etri/sl/configs/Config.java
@@ -3,11 +3,11 @@ package com.etri.sl.configs;
 public class Config {
     // TODO modify Gateway Object
     // TEST API URI
-    private String BASE_URI = "http://192.168.0.2:9000/gw/v1";
+    public static String BASE_URI = "http://13.124.195.114:9000/gw/v1";
 
     // IBM watson conversation API
     public static String endpoint = "https://gateway.watsonplatform.net/conversation/api";
-    public static String username = "username";
-    public static String password = "password";
-    public static String workspaceId = "workspaceId";
+    public static String username = "448c149b-849c-4525-901f-efa73421f5f1";
+    public static String password = "Ov46ffwJAw5A";
+    public static String workspaceId = "068cc140-51b8-4d0d-9e4b-ac06439ac69c";
 }
